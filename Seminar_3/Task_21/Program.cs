240f5260030ccb4dbc738f9b9b04b8510db2d6da
Damir Makaev <makaev_d@mail.ru>
2023-07-01 22:37:12 +0400

    Сделал задачу № 21 домашнего задания третьего семинара

--- Seminar_3/Task_21/Program.cs
+++ Seminar_3/Task_21/Program.cs
@@ -4,32 +4,22 @@
 // A (3,6,8); B (2,1,-7), -> 15.84
 // A (7,-5, 0); B (1,-1,9) -> 11.53
 
-int InputN(string message)
-{
-System.Console.Write($"{message}> ");
-int value;
-if (int.TryParse(Console.ReadLine(), out value))
-{
-return value;
-}
-System.Console.WriteLine("Вы ввели не число");
-Environment.Exit(1);
-return 0;
+Console.Write("Введите координату X первой точки A: ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату Y первой точки A: ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату Z первой точки A: ");
+int z1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату X второй точки B: ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату Y второй точки B: ");
+int y2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату Z второй точки B: ");
+int z2 = Convert.ToInt32(Console.ReadLine());
 
-}
+double skobX = Math.Pow(x1 - x2, 2);
+double skobY = Math.Pow(y1 - y2, 2);
+double skobZ = Math.Pow(z1 - z2, 2);
+double result = Math.Sqrt(skobX + skobY + skobZ);
 
-int x1 = InputN ("Введите координату X первой точки A");
-int y1 = InputN ("Введите координату Y первой точки A");
-int z1 = InputN ("Введите координату Z первой точки A");
-int x2 = InputN ("Введите координату X второй точки B");
-int y2 = InputN ("Введите координату Y второй точки B");
-int z2 = InputN ("Введите координату Z второй точки B");
-
-double dis(int x1, int y1, int z1, int x2, int y2, int z2)
-{
-int deltax = x2 — x1;
-int deltay = y2 — y1;
-int deltaz = z2 — z1;
-return Math.Sqrt(deltax*deltax+deltay*deltay+deltaz*deltaz);
-}
-System.Console.Write($"Растояние между точками A и B равно {dis(x1, y1, z1, x2, y2, z2):F2} ");
+Console.Write($"Растояние между точками A и B равно: {result:f2}");
