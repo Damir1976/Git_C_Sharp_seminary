379d964693c855e3d003191e595ce31652fa40ba
Damir Makaev <makaev_d@mail.ru>
2023-07-02 16:11:41 +0400

    Сделал задачу № 29 домашнего задания четвертого семинара

--- Seminar_4/Task_29/Program.cs
+++ Seminar_4/Task_29/Program.cs
@@ -1,9 +1,15 @@
 ﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
 
-int[] randomArray = new int[8];
+int[] array = new int[8];
 
+Console.WriteLine("Введите 8 произвольных чисел в массив: ");
 for (int i = 0; i < 8; i++)
 {
-randomArray[i] = new Random().Next(1,99);
-Console.Write(randomArray[i] + » «);
+    Console.Write($"{(i+1)} число: ");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+Console.Write("У нас получился вот такой массив: ");
+for (int i = 0; i < 8; i++)
+{
+    Console.Write($"{array[i]}, ");
 }
