f77699d05413dae69d8a3242ba1b992f359b7f25
Damir Makaev <makaev_d@mail.ru>
2023-07-03 14:55:03 +0400

    Сделал задачу № 38 домашнего задания пятого семинара

--- Seminar_5/Task_38/Program.cs
+++ Seminar_5/Task_38/Program.cs
@@ -3,37 +3,37 @@
 //            (не использовать стандартные функции/методы).
 //            [3 7 22 2 78] -> 76
 
-Console.Write("Введите количество элементов массива: ");
+Console.Write("Введите количество элементов из которых состоит массив: ");
 int a = Convert.ToInt32(Console.ReadLine());
-int[] array = GetRandomArray();
+int[] array = new int[a];
 
-void mas(int a)
+void Chisla(int a)
 {
-    Random rand = new Random();
+    Console.WriteLine("Введите произвольные числа в массив - ");
     for (int i = 0; i < a; i++)
     {
-        randomArray[i] = new Random().Next(1, 100);
-        Console.Write($"{randomArray[i]:F2} ");
+        Console.Write($"{(i + 1)} число: ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
     }
 }
 
-int raz(int[] randomArray)
+int Raznica(int[] array)
 {
-    int min = randomArray[0];
-    int max = randomArray[0];
-    int i = 1;
-    while (i < randomArray.Length)
+    int min = array[0];
+    int max = array[0];
+    for (int i = 1; i < array.Length; i++)
     {
-        if (max < randomArray[i])
-            max = randomArray[i];
-        if (min > randomArray[i])
-            min = randomArray[i];
-        i = i + 1;
+        if (max < array[i])
+        {
+            max = array[i];
+        }
+        if (min > array[i])
+        {
+            min = array[i];
+        }
     }
     return max - min;
 }
 
-mas(a);
-Console.Write(
-    $"\nРазница между максимальным и минимальным элементов массива: {raz(randomArray):F2}"
-);
+Chisla(a);
+Console.Write($"Разница между максимальным и минимальным элементами массива: {Raznica(array)}");
