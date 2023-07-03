1600a041276f5c23fae3960c3377983cb92522a5
Damir Makaev <makaev_d@mail.ru>
2023-07-03 13:34:41 +0400

    Сделал задачу № 34 домашнего задания пятого семинара

---
+++ Seminar_5/Task_34/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами.
+//            Напишите программу, которая покажет количество чётных чисел в массиве.
+//            [345, 897, 568, 234] -> 2
+
+Console.Write("Задайте количество элементов из которых состоит массив: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[a];
+
+void massiv(int a)
+{
+    for (int i = 0; i < a; i++)
+    {
+        array[i] = Random.Shared.Next(100, 1000);
+        Console.Write(array[i] + ", ");
+    }
+}
+
+int chetnye(int[] array)
+{
+    int chetnye = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+            chetnye = chetnye + 1;
+    }
+    return chetnye;
+}
+
+massiv(a);
+Console.Write($"\nКоличество чётных чисел в массиве: {chetnye(array)}");
---
+++ Seminar_5/Task_38/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 38: Задайте массив натуральных чисел.
+//            Найдите разницу между максимальным и минимальным элементом массива.
+//            (не использовать стандартные функции/методы).
+//            [3 7 22 2 78] -> 76
+
+Console.Write("Введите количество элементов массива: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int[] array = GetRandomArray();
+
+void mas(int a)
+{
+    Random rand = new Random();
+    for (int i = 0; i < a; i++)
+    {
+        randomArray[i] = new Random().Next(1, 100);
+        Console.Write($"{randomArray[i]:F2} ");
+    }
+}
+
+int raz(int[] randomArray)
+{
+    int min = randomArray[0];
+    int max = randomArray[0];
+    int i = 1;
+    while (i < randomArray.Length)
+    {
+        if (max < randomArray[i])
+            max = randomArray[i];
+        if (min > randomArray[i])
+            min = randomArray[i];
+        i = i + 1;
+    }
+    return max - min;
+}
+
+mas(a);
+Console.Write(
+    $"\nРазница между максимальным и минимальным элементов массива: {raz(randomArray):F2}"
+);
