f4abd8b9c5fa0436cfc1c02ef429ddbe02bc1fd8
Damir Makaev <makaev_d@mail.ru>
2023-07-07 12:14:58 +0400

    Сделал задачу № 47 домашнего задания седьмого семинара

---
+++ Seminar_7/Task_47/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+//             m = 3, n = 4.
+//             0,5 7 -2 -0,2
+//             1 -3,3 8 -9,9
+//             8 7,8 -7,1 9
+
+
+Console.Write("Введите размерность двумерного массива по строкам (M): ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите размерность двумерного массива по столбцам (N): ");
+int n = Convert.ToInt32(Console.ReadLine());
+double[,] randomArray = new double[m, n];
+
+void massiv(int m, int n)
+{
+    Random rand = new Random();
+    Console.Write($"Наш массив:");
+    for (int i = 0; i < m; i++)
+    {
+        Console.WriteLine();
+        for (int j = 0; j < n; j++)
+        {
+            randomArray[i, j] = rand.Next(-100, 100) / 10.0;
+            Console.Write($"{randomArray[i, j]:F1} ");
+        }
+    }
+}
+
+massiv(m, n);
---
+++ Seminar_7/Task_50/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве,
+//            и возвращает значение этого элемента или же указание, что такого элемента нет.
+//             Например, задан массив:
+//             1 4 7 2
+//             5 9 2 3
+//             8 4 2 4
+//             17 -> такого числа в массиве нет
+
+Console.Write("Введите размерность двумерного массива по M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите размерность двумерного массива по N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+double[,] randomArray = new double[m, n];
+Console.Write("Введите позицию строки: ");
+int m2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите позицию столбца: ");
+int n2 = Convert.ToInt32(Console.ReadLine());
+void mas(int m, int n)
+{
+    int i,
+        j;
+    Random rand = new Random();
+    for (i = 0; i < m; i++)
+    {
+        // Console.WriteLine();
+        for (j = 0; j < n; j++)
+        {
+            // randomArray[i, j] = rand.NextDouble();
+            randomArray[i, j] = Random.Shared.Next(1, 10);
+            Console.Write($"{randomArray[i, j]}, ");
+        }
+        Console.WriteLine();
+    }
+}
+mas(m, n);
+if (m2 < 0 || n2 < 0)
+    Console.Write("Позиции строк не могут быть отрицательными");
+else if (m2 <= m + 1 && n2 <= n + 1)
+    Console.Write($"Значение элемента равно {randomArray[m2 - 1, n2 - 1]} ");
+else
+    Console.Write("Такого элемента нет в массиве");
