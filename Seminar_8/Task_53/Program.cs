30c2008fe85858f37ad491cd38f2ab74f4c3f8ed
Damir Makaev <makaev_d@mail.ru>
2023-07-07 14:46:00 +0400

    Сделал задачу № 54 домашнего задания восьмого семинара

---
+++ Seminar_8/Task_53/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 53: Задайте двумерный массив. Напишите программу, которая поменяет местами первую 
+//            и последнюю строку массива.
+//              1 3 5 7
+//              2 8 9 6 
+//              3 5 7 9 
+//              5 1 2 8
+//              6 9 1 3
+
+
+
+
+
+const int ROWS = 5;
+const int COLUMNS = 4;
+int[,] GetRandomMatrix(int rows, int columns)
+{
+    int[,] matrix = new int[rows, columns];
+    for(int m = 0; m < matrix.GetLength(0); m++)
+    {
+        for(int n = 0; n < matrix.GetLength(1); n++)
+        {
+            matrix[m, n] = m + n;
+        }
+    }
+
+    return matrix;
---
+++ Seminar_8/Task_54/Program.cs
@@ -0,0 +1,66 @@
+﻿// Задача 54:  Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию
+//             элементы каждой строки двумерного массива.
+//             Например, задан массив:
+//             1 4 7 2
+//             5 9 2 3
+//             8 4 2 4
+//             В итоге получается вот такой массив:
+//             7 4 2 1
+//             9 5 3 2
+//             8 4 4 2
+
+Console.Write("Введите размерность двумерного массива по строкам (M): ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите размерность двумерного массива по столбцам (N): ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] randomArray = new int[m, n];
+
+void massiv(int m, int n)
+{
+    Random rand = new Random();
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            randomArray[i, j] = rand.Next(1, 10);
+        }
+    }
+}
+
+void printmassiv(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void sortmassiv(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(1) - 1; k++)
+            {
+                if (array[i, k] < array[i, k + 1])
+                {
+                    int a = array[i, k + 1];
+                    array[i, k + 1] = array[i, k];
+                    array[i, k] = a;
+                }
+            }
+        }
+    }
+}
+
+massiv(m, n);
+Console.WriteLine("Сгеренированный массив: ");
+printmassiv(randomArray);
+sortmassiv(randomArray);
+Console.WriteLine("Упорядоченный массив: ");
+printmassiv(randomArray);
