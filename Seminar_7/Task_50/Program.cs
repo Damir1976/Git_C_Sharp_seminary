a11e225311ebec40f911896c2894f5103fd71a34
Damir Makaev <makaev_d@mail.ru>
2023-07-07 12:42:16 +0400

    Сделал задачу № 50 домашнего задания седьмого семинара

--- Seminar_7/Task_50/Program.cs
+++ Seminar_7/Task_50/Program.cs
@@ -6,24 +6,22 @@
 //             8 4 2 4
 //             17 -> такого числа в массиве нет
 
-Console.Write("Введите размерность двумерного массива по M: ");
+Console.Write("Введите размерность двумерного массива по строкам (M): ");
 int m = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите размерность двумерного массива по N: ");
+Console.Write("Введите размерность двумерного массива по столбцам (N): ");
 int n = Convert.ToInt32(Console.ReadLine());
 double[,] randomArray = new double[m, n];
 Console.Write("Введите позицию строки: ");
-int m2 = Convert.ToInt32(Console.ReadLine());
+int positionM = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите позицию столбца: ");
-int n2 = Convert.ToInt32(Console.ReadLine());
-void mas(int m, int n)
+int positionN = Convert.ToInt32(Console.ReadLine());
+void massiv(int m, int n)
 {
-    int i,
-        j;
     Random rand = new Random();
-    for (i = 0; i < m; i++)
+    for (int i = 0; i < m; i++)
     {
         // Console.WriteLine();
-        for (j = 0; j < n; j++)
+        for (int j = 0; j < n; j++)
         {
             // randomArray[i, j] = rand.NextDouble();
             randomArray[i, j] = Random.Shared.Next(1, 10);
@@ -32,10 +30,12 @@ void mas(int m, int n)
         Console.WriteLine();
     }
 }
-mas(m, n);
-if (m2 < 0 || n2 < 0)
-    Console.Write("Позиции строк не могут быть отрицательными");
-else if (m2 <= m + 1 && n2 <= n + 1)
-    Console.Write($"Значение элемента равно {randomArray[m2 - 1, n2 - 1]} ");
-else
-    Console.Write("Такого элемента нет в массиве");
+massiv(m, n);
+if (positionM <= m - 1 && positionN <= n - 1)
+{
+    Console.Write($"Значение элемента = {randomArray[positionM - 1, positionN - 1]} ");
+}
+if (positionM > m - 1 || positionN > n - 1)
+{
+    Console.Write("Такого элемента в массиве нет");
+}
