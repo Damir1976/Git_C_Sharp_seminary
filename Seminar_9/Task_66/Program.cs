aa31f9e1ef227910014092c758172f49c428b478
Damir Makaev <makaev_d@mail.ru>
2023-07-11 18:36:37 +0400

    Сделал задачу № 66 домашнего задания девятого семинара

--- Seminar_9/Task_66/Program.cs
+++ Seminar_9/Task_66/Program.cs
@@ -3,3 +3,14 @@
 //             M = 1; N = 15 -> 120
 //             M = 4; N = 8. -> 30
 
+Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int Summa(int m, int n)
+{
+    if (n <= m) return n;
+        return m + Summa (m + 1, n);
+}
+Console.Write($"Сумма натуральных элементов в промежутке от {m} до {n} = " + Summa (m, n)); 
