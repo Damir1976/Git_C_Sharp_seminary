5ef31eee6b78538249eeb4269e5c161e468e1f60
Damir Makaev <makaev_d@mail.ru>
2023-07-11 17:57:01 +0400

    Сделал задачу № 64 домашнего задания девятого семинара

---
+++ Seminar_9/Task_64/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 64:   Задайте значение N. Напишите программу, которая выведет все натуральные числа
+//              в промежутке от N до 1. Выполнить с помощью рекурсии.
+//              N = 5 -> "5, 4, 3, 2, 1"
+//              N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+Console.Write("Введите число N: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+void Perechislenie(int number)
+{
+    if (number == 1)
+        return;
+        Console.Write(number + ", ");
+        Perechislenie (number-1);
+}
+
+Console.Write("Натуральные числа в промежутке от N до 1 -> "); 
+Perechislenie(number);
+
