f9b3519107e1951e2330d50de0783dff5767f43f
Damir Makaev <makaev_d@mail.ru>
2023-07-11 14:49:37 +0400

    Сделал задачу № 60 домашнего задания восьмого семинара

--- Seminar_8/Task_60/Program.cs
+++ Seminar_8/Task_60/Program.cs
@@ -5,3 +5,72 @@
 //             34(1,0,0) 41(1,1,0)
 //             27(0,0,1) 90(0,1,1)
 //             26(1,0,1) 55(1,1,1)
+
+Console.WriteLine("Задайте размерность трехмерного массива по M, по N и по O - ");
+int m = InputNumbers("Введите M: ");
+int n = InputNumbers("Введите N: ");
+int o = InputNumbers("Введите O: ");
+int[,,] array = new int[m, n, o];
+
+int InputNumbers(string input)
+{
+    Console.Write(input);
+    int output = Convert.ToInt32(Console.ReadLine());
+    return output;
+}
+
+void WriteArray(int[,,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                Console.Write($"{array[i, j, k]} ({i},{j},{k}); ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
+void CreateArray(int[,,] array)
+{
+    int[] temp = new int[array.GetLength(0) * array.GetLength(1) * array.GetLength(2)];
+    int number;
+    for (int i = 0; i < temp.GetLength(0); i++)
+    {
+        temp[i] = new Random().Next(10, 100);
+        number = temp[i];
+        if (i >= 1)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                while (temp[i] == temp[j])
+                {
+                    temp[i] = new Random().Next(10, 100);
+                    j = 0;
+                    number = temp[i];
+                }
+                number = temp[i];
+            }
+        }
+    }
+    int count = 0;
+    for (int m = 0; m < array.GetLength(0); m++)
+    {
+        for (int n = 0; n < array.GetLength(1); n++)
+        {
+            for (int o = 0; o < array.GetLength(2); o++)
+            {
+                array[m, n, o] = temp[count];
+                count++;
+            }
+        }
+    }
+}
+
+CreateArray(array);
+Console.WriteLine("Полученный массив: ");
+WriteArray(array);
+
